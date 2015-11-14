7f7769c81c89a00c4b19ad4097bba7c77a1dafdb
faisalmohd <faisalhmohd@live.com>
2015-11-14 10:51:53 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = process.env.port || 3000;
+
+app.get('/', function(req, res){
+  res.send('Routed properly');
+});
+
+io.on('connection', function(socket){
+  console.log('User connected');
+
+  socket.on('disconnect', function(){
+  console.log('User disconnected');
+  });
+
+  socket.on('new message', function(msg){
+  console.log(msg.user + ': ' + msg.message);
+  io.emit('new message', msg);
+  });
+
+  socket.on('new user', function(msg){
+  console.log(msg + ' has just joined');
+  io.emit('new user', msg);
+  });
+});
+
+app.listen(port, function() {
+    console.log('The server has been initiated at ' + port );
+});
