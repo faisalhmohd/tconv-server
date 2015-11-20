766f9a13c1d2404ddc65fd1e66ad1bd61ce2db57
faisalmohd <faisalhmohd@live.com>
2015-11-20 15:56:06 +0600

    Heroku publishing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.send('Routed properly');
